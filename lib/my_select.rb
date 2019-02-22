c9c93ea86603e08b42e0a8f14ba82f8aeb24ff3c
manon jacquin <manon.jacquin@gmail.com>
2019-02-22 12:09:30 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,4 +1,12 @@
 def my_select(collection)
  # your code here!
- 
+ counter = 0 
+ new_collection = [] 
+ while counter < collection 
+  if yield(collection[counter]) ==  true 
+    new_collection << collection[counter]
+  end 
+  counter += 1 
+end
+new_collection
 end
