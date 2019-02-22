7c6feb6892bd91d707f9f9cac2ae648d292077eb
manon jacquin <manon.jacquin@gmail.com>
2019-02-22 12:11:14 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
  # your code here!
+ counter = 0 
+ new_collection = [] 
+ while counter < collection.length 
+ # checks if the condition in the block is true
+  if yield(collection[counter]) ==  true 
+    # pushes the values in the new collection
+    new_collection << collection[counter]
+  end 
+  counter += 1 
+end
+new_collection
 end
